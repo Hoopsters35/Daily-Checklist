1c5a3dfc888d717ad37f529441433222c67d6a11
Justin Knueppel <justinknueppel@gmail.com>
2018-06-26 20:30:58 -0400

    Add missing semi-colons

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ app = {};
 app.init = function() {
     $('button #add-task').click(() => {
         app.newTask();
-    })
-}
+    });
+};
 
 app.newTask = function() {
     
-}
+};
 
 $(app.init);
