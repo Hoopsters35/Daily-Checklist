93909f19c57bc4d457d62014fab500f2975ca32b
= <=>
2018-08-19 09:55:19 -0400

    Add a correct curl command to notes to test POST

--- Backend/api.py
+++ Backend/api.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from flask_restful import Api, Resource
+
+# curl command to test POST
+# curl -i -H "Content-Type: application/json" -X POST -d "{\"no\": \"way\"}" http://127.0.0.1:5000/ 
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,8 +12,8 @@ class HelloWorld(Resource):
         return {"data": "Hello World"}
 
     def post(self):
-        some_json = request.get_json()
-        return f"You sent: {some_json}"
+        some_json = request.get_json(force=True)
+        return {"sent_data": f"You sent: {some_json}"}
 
 api.add_resource(HelloWorld, '/')
 
